f48ce92c3c27aefbec6f28221ff259d04e5250f3
Nikhil Gupta <nikgupta06@gmail.com>
2023-03-29 01:21:02 -0500

    Complete image conversion for a given 24-bit bitmap file

--- ImageRgbToGray/main.cpp
+++ ImageRgbToGray/main.cpp
@@ -3,20 +3,19 @@
 
 struct Args
 {
-    char* input;
-    char* output;
-    char* mode;
+    char *input;
+    char *output;
+    char *mode;
 };
 
 Args parseArgs(int argc, char *argv[]);
-
 
 int main(int argc, char *argv[])
 {
     std::cout << "Running CPU ImageRgbToGray program..." << std::endl;
     Args args = parseArgs(argc, argv);
 
-    std::cout << "Loading image..." << std::endl;
+    std::cout << "Loading image from '" << args.input << "'..." << std::endl;
     bitmap_image img(args.input);
     if (!img)
     {
@@ -25,27 +24,56 @@ int main(int argc, char *argv[])
     }
     const int h = img.height();
     const int w = img.width();
-    rgb_t pixels[h][w];
-    for(int i = 0; i < h; i++)
-        for(int j = 0; j < w; j++)
-            img.get_pixel(i, j, pixels[i][j]);
 
-    std::cout << "Converting image..." << std::endl;
+    std::cout << "Converting image with mode '" << args.mode << "'..." << std::endl;
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            rgb_t pixel = img.get_pixel(i, j);
+            int value;
+            if (strcmp(args.mode, "lightness") == 0)
+            {
+                int min = __min(pixel.red, __min(pixel.green, pixel.blue));
+                int max = __max(pixel.red, __max(pixel.green, pixel.blue));
+                value = min + (max - min) / 2;
+            }
+            else if (strcmp(args.mode, "average") == 0)
+            {
+                value = (pixel.red + pixel.green + pixel.blue) / 3;
+            }
+            else if (strcmp(args.mode, "luminosity") == 0)
+            {
+                value = (0.3 * pixel.red + 0.59 * pixel.green + 0.11 * pixel.blue);
+            }
+            else
+            {
+                std::cerr << "Unsupported mode '" << args.mode << "'." << std::endl;
+                exit(1);
+            }
 
+            img.set_pixel(i, j, value, value, value);
+        }
+    }
+
+    std::cout << "Outputting image to '" << args.output << "'..." << std::endl;
+    img.save_image(args.output);
 }
 
-Args parseArgs(int argc, char *argv[]) {
+Args parseArgs(int argc, char *argv[])
+{
     // Expect 3 or 4 total args
     if (argc < 3 || argc > 4)
     {
-        std::cerr << "Must provide an input filename, output filename, and " 
-            << "optionally a grayscale mode to use (one of lightness, average, and luminosity)." << std::endl;
+        std::cerr << "Must provide an input filename, output filename, and "
+                  << "optionally a grayscale mode to use (lightness, average, and luminosity (default))." << std::endl;
         exit(1);
     }
 
-    return {
-        argv[1], 
-        argv[2], 
-        (argc == 4) ? argv[3] : nullptr
-    };
+    Args args = {argv[1], argv[2]};
+    if (argc == 4)
+        args.mode = argv[3];
+    else
+        args.mode = (char *)"luminosity";
+    return args;
 }
